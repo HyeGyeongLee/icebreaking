50969703b5f0e3a67e8e6d2f6f9e6eddf825a676
LeeHyeGyeong <wingedant1@naver.com>
2023-07-13 14:22:42 +0900

    footer 높이 수정

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <div className="relative h-screen xl:h-full lg:h-full md:h-full sm:h-full w-full">
+      <div className="relative">
         <div className="lg:mx-96 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center">
             ⓒ 2020. HyeGyeongLee all rights reserved.
         </div>
