0fa34c290b90abea9819d7e914129e9fcf30c7db
LeeHyeGyeong <wingedant1@naver.com>
2023-07-13 14:20:02 +0900

    copyright 추가

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
     return (
-      <div>
-        footer~~~
+      <div className="relative h-screen xl:h-full lg:h-full md:h-full sm:h-full w-full">
+        <div className="lg:mx-96 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center">
+            ⓒ 2020. HyeGyeongLee all rights reserved.
+        </div>
       </div>
     )
 }
