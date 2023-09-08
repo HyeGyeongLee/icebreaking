b49b567c36df2940a87f57b47a3303f47c2345f4
LeeHyeGyeong <wingedant1@naver.com>
2023-09-08 16:51:21 +0900

    build 오류 잡는 중 - supabase페이지 수정 중

--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -5,4 +5,7 @@ const supabaseAnonKey = process.env.VITE_REACT_APP_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
+if (!supabaseUrl) throw new Error('supabaseUrl is required.')
+if (!supabaseAnonKey) throw new Error('supabaseKey is required.')
+
 export default supabase;
