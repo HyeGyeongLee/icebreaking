cc462d2a516798c977c666eb99f3c7469bdab6ed
LeeHyeGyeong <wingedant1@naver.com>
2023-09-08 12:57:17 +0900

    메뉴 tab 추가

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,1 +1,36 @@
-export default function Header() { }
+export default function Header() {
+    const tabs = [
+        { name: 'HOME', href: '#', current: true },
+        { name: 'GAME', href: '#', current: false },
+        { name: 'LOGIN', href: '#', current: false },
+      ]
+      
+    return(
+        <>
+        <div className="absolute top-[12px]">
+        <div className=" px-4 py-6 px-6">
+            <div className="mx-auto max-w-7xl">
+                <div className="block">
+                <nav className="flex py-4">
+                    <ul
+                    role="list"
+                    className="flex min-w-full flex-none gap-x-6 px-2 text-2xl font-semibold leading-6 text-gray-400"
+                    >
+                    {tabs.map((tab) => (
+                        <li key={tab.name}>
+                        <a href={tab.href} className={tab.current ? 'text-indigo-400' : ''}>
+                            {tab.name}
+                        </a>
+                        </li>
+                    ))}
+                    </ul>
+                </nav>
+                </div>
+            </div>
+            </div>
+
+        </div>
+        </>
+    )
+
+ }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { CheckIcon } from '@heroicons/react/20/solid'
+import Header from "../components/layout/Header"
 
 const includedFeatures = [
   'Private forum access',
@@ -12,8 +13,8 @@ export default function Ice() {
     return (
       <div className="relative h-screen xl:h-full lg:h-full md:h-full sm:h-full w-full">
         <div className="lg:mx-96 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center">
-            
             <div className="relative bg-[#FFFBDC] h-screen sm:pb-[1200px] md:pb-[1200px] lg:pb-[1200px] xl:pb-[1200px]">
+            <Header />
               <img src="./icebreaking_title.png"/>
               <div className="relative mx-12 -my-52 sm:-my-72 md:-my-72 lg:-my-72 xl:-my-72">
                   <Link href="/icebreak/question">
