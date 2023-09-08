add033a3b560496f8a6295ef2b48a83dc4f2066e
LeeHyeGyeong <wingedant1@naver.com>
2023-09-08 17:13:44 +0900

    빌드 에러 잡는 중 - superbase 파일 수정 중/ 이름 오타

--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.VITE_REACT_APP_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.VITE_REACT_APP_ANON_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
