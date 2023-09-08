42191c7f18d80f37a13d4d225917d28884597dd4
LeeHyeGyeong <wingedant1@naver.com>
2023-09-08 16:30:09 +0900

    build 오류 잡는 중 - supabase페이지 수정 중

--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseServerKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
+const supabaseUrl: string = process.env.PUBLIC_SUPABASE_URL || '';
+const supabaseServerKey: string = process.env.PUBLIC_SUPABASE_KEY || '';
 
 const supabase = createClient(supabaseUrl, supabaseServerKey);
 
